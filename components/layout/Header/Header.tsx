2f690ad493e7a49e2be9135c1b0bd6a48f46bf1f
dev-nolan-ayer <dev.nolan.ayer@gmail.com>
2023-10-02 12:38:00 -0700

    conditionally render promo slider on only home page

--- components/layout/Header/Header.tsx
+++ components/layout/Header/Header.tsx
@@ -1,12 +1,16 @@
+"use client";
+
+import { useRouter } from "next/router";
 import styles from "./Header.module.css";
 import HeaderNav from "./HeaderNav";
 import HeaderPromo from "./HeaderPromo";
 
 export default function Header() {
+  const { pathname } = useRouter;
   return (
     <div className={styles.header}>
       <HeaderNav />
-      <HeaderPromo />
+      {pathname == "/" && <HeaderPromo />}
     </div>
   );
 }
