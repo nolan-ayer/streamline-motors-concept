0650546cc5fba2909e619b18f31d9b332b603442
dev-nolan-ayer <dev.nolan.ayer@gmail.com>
2023-10-19 00:22:12 -0700

    imported stylind from findCar component

--- components/ui/Search/Refine.tsx
+++ components/ui/Search/Refine.tsx
@@ -3,77 +3,82 @@ import Select from "../Input/Select";
 
 export default function Refine() {
   return (
-    <form>
-      <h1>REFINE</h1>
-      <div className={styles.findCarColumn}>
-        <div className={styles.findCarLabel}>Year Min</div>
-        <input type="number" placeholder="None" />
-      </div>
-      <div className={styles.findCarColumn}>
-        <div className={styles.findCarLabel}>Year Max</div>
-        <input type="number" placeholder="None" />
-      </div>
-      <div className={styles.findCarColumn}>
-        <div className={styles.findCarLabel}>Make</div>
-        <Select
-          config={{
-            defaultValue: "Placeholder",
-            innerText: [
-              "Any",
-              "Audi",
-              "BMW",
-              "Bugatti",
-              "Chevrolet",
-              "Chrysler",
-              "Ferrari",
-              "Fiat",
-              "Ford",
-              "Honda",
-              "Hyundai",
-              "Jaguar",
-              "Jeep",
-              "Kia",
-              "Lamborghini",
-              "Land Rover",
-              "Lexus",
-              "Maserati",
-              "Mazda",
-              "McLaren",
-              "Mercedes-Benz",
-              "Mitsubishi",
-              "Nissan",
-              "Porsche",
-              "Ram",
-              "Subaru",
-              "Tesla",
-              "Toyota",
-              "Volkswagen",
-              "Volvo",
-            ],
-            inputValue: "make",
-          }}
-        />
-      </div>
-      <div className={styles.findCarColumn}>
-        <div className={styles.findCarLabel}>Model</div>
-        <select>
-          <option value="model">Any</option>
-        </select>
-      </div>
-      <div className={styles.findCarColumn}>
-        <div className={styles.findCarLabel}>Body</div>
-        <Select
-          config={{
-            defaultValue: "Placeholder",
-            innerText: ["Any", "Coupe", "Sedan", "SUV", "Truck", "Wagon"],
-            inputValue: "body",
-          }}
-        />
-      </div>
-      <div className={styles.findCarColumn}>
-        <div className={styles.findCarLabel}>Price Max</div>
-        <input type="number" placeholder="None" />
-      </div>
-    </form>
+    <div className={styles.findCar}>
+      <form className={styles.findCarForm}>
+        <h1>REFINE</h1>
+        <div className={styles.findCarColumn}>
+          <div className={styles.findCarLabel}>Year Min</div>
+          <input type="number" placeholder="None" />
+        </div>
+        <div className={styles.findCarColumn}>
+          <div className={styles.findCarLabel}>Year Max</div>
+          <input type="number" placeholder="None" />
+        </div>
+        <div className={styles.findCarColumn}>
+          <div className={styles.findCarLabel}>Make</div>
+          <Select
+            config={{
+              defaultValue: "Placeholder",
+              innerText: [
+                "Any",
+                "Audi",
+                "BMW",
+                "Bugatti",
+                "Chevrolet",
+                "Chrysler",
+                "Ferrari",
+                "Fiat",
+                "Ford",
+                "Honda",
+                "Hyundai",
+                "Jaguar",
+                "Jeep",
+                "Kia",
+                "Lamborghini",
+                "Land Rover",
+                "Lexus",
+                "Maserati",
+                "Mazda",
+                "McLaren",
+                "Mercedes-Benz",
+                "Mitsubishi",
+                "Nissan",
+                "Porsche",
+                "Ram",
+                "Subaru",
+                "Tesla",
+                "Toyota",
+                "Volkswagen",
+                "Volvo",
+              ],
+              inputValue: "make",
+            }}
+          />
+        </div>
+        <div className={styles.findCarColumn}>
+          <div className={styles.findCarLabel}>Model</div>
+          <select>
+            <option value="model">Any</option>
+          </select>
+        </div>
+        <div className={styles.findCarColumn}>
+          <div className={styles.findCarLabel}>Body</div>
+          <Select
+            config={{
+              defaultValue: "Placeholder",
+              innerText: ["Any", "Coupe", "Sedan", "SUV", "Truck", "Wagon"],
+              inputValue: "body",
+            }}
+          />
+        </div>
+        <div className={styles.findCarColumn}>
+          <div className={styles.findCarLabel}>Price Max</div>
+          <input type="number" placeholder="None" />
+        </div>
+        <button className={styles.submitButton} type="submit">
+          Submit
+        </button>
+      </form>
+    </div>
   );
 }
